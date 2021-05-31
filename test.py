e3dd3e89b45a35399f54a5ca5bf7e8297eac5dbe
Does-a-thingy <79290020+Does-a-thingy@users.noreply.github.com>
2021-05-31 14:58:53 +1200

    Update test.py

--- test.py
+++ test.py
@@ -12,3 +12,14 @@ while x <= 10:
     if x == 6:
         print('------')
 
+name = input('what is your name?')
+print('hi ' + name)
+year = int(input('what year is it?'))
+self_age = int(input('what is your age?')
+father_age = int(input('how old is your father?'))
+mother_age = int(input('how old is your mother?'))
+early_year = year - self_age
+late_year = early_year - 1
+
+
+print('you were born in {} or {}. your father was {} when you were born and your mother was {}. your father is {} years older than your mother.'.format(early_year, late_year, father_born_age, mother_born_age, father_mother_age))
