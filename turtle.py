dc962fd5033d43818e9c6d3d787c3d3c2dc340c2
Does-a-thingy <79290020+Does-a-thingy@users.noreply.github.com>
2021-06-01 19:48:11 +1200

    Create turtle.py

---
+++ turtle.py
@@ -0,0 +1,23 @@
+import turtle
+sara = turtle.turtle()
+sara.shape("turtle")
+
+khris = sara.clone()
+
+khris.pencolor("red")
+Khris.circle(30)
+
+sara.pencolor("yellow")
+sara.lt(180)
+sara.circle(15)
+
+sara.penup()
+sara.goto(30, 0)
+sara.pendown()
+sara.fd(20)
+sara.rt(45)
+sara.fd(10)
+sara.lt(50)
+sara.fd(17)
+sara.lt(85)
+sara.bk(27)
