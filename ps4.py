6bcb2a8b7f7e2911d95fed181281b15cf333feef
Does-a-thingy <79290020+Does-a-thingy@users.noreply.github.com>
2021-06-29 09:57:46 +1200

    discount

--- ps4.py
+++ ps4.py
@@ -1,23 +1,25 @@
-# Set up price variables
 ps3_game = 17
 ps4_game = 45
 
-#Temporary discount on PS3 games
-
-
-#Ask for number of each game to be purchased
 num_ps3_games = int(input("How many PS3 games?: "))
 num_ps4_games = int(input("How many PS4 games?: "))
 
-#Calculate total for each type of game
 ps3_total = num_ps3_games * ps3_game
 ps4_total = num_ps4_games * ps4_game
 
-#Calculate total cost
-total_cost = ps3_total + ps4_total
+total = ps3_total + ps4_total
 
-#Apply 15% discount
-total_cost *= 0.85
-
+if total > 100 and < 200:
+  total *= 0.95
+  print("you get a 5% discount for spending over 100 dollars")
+  #5% discount
+elif total > 200 and < 300:
+  total *= 0.9
+	print("you get a 10% discount for spending over 200 dollars")
+  #10% discount
+elif total > 300:
+  total *= 0.8
+	print("you get a 20% discount for spending over 300 dollars")
+  #20% discount
 #Print out total order cost
 print("You order costs: ${}".format(total_cost))
