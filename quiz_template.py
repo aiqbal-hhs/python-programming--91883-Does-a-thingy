4980ccb5c960077c3471470b890506051bb2e1ca
Oliver <Oliver.macclure@student.hhs.school.nz>
2021-08-03 09:10:38 +1200

    Update quiz_template.py

--- quiz_template.py
+++ quiz_template.py
@@ -3,8 +3,9 @@ import time
 
 score = 0
 
+questions = ["What is you name?", "Are we ready to start?", "Who is the king of the Greek titans? Kronos, Zeus, Gaea, or Saturn?",  "Name one famous ancient Greek philosopher.",]
 # asking name and welcome print
-name = input("What is you name?")
+name = input(questions[0])
 print("Welcome to this quiz {}!".format(name))
 
 # Quiz info and topic
@@ -106,5 +107,47 @@ fifthq.lower()
         print("""That is wrong,
 crete is a greek island that was home to the wealthist
 city-state of ancient greece.""")
-elif fifthq == "the garden of hesperides":
+    elif fifthq == "the garden of hesperides":
         print("That is incorrect, that is Hera's orchard which is found near portugal")
+    else:
+        print("Thats not one of the answers")
+
+time.sleep(2)
+
+# Sixth Question - multi answer typing
+print("Multiple answer typed question")
+sixthq = input("Name one famous ancient Greek philosopher.")
+sithq.strip()
+sithq.lower()
+    if sixthq == "socrates":
+        print("That is correct, he is the founder of western philosophy and the first moral philosopher.")
+        score += 1
+    elif sixthq == "plato":
+        print("That is correct, an innovator of the written dialogue and dialectic forms in philosophy.")
+        score += 1
+    elif sixthq == "aristotle":
+        print("That is correct.")
+        score += 1
+    elif sixthq == "thale":
+        print("That is correct.")
+        score += 1
+    elif sixthq == "pythagoras":
+        print("That is correct.")
+        score += 1
+    elif sixthq == "democritus":
+        print("That is correct.")
+        score += 1
+    elif sixthq == "empedokles":
+        print("That is correct.")
+        score += 1
+    elif sixthq == "anaxagoras":
+        print("That is correct.")
+        score += 1
+    elif sixthq == "anaximander":
+        print("That is correct.")
+        score += 1
+    elif sixthq == "epicurus":
+        print("That is correct.")
+        score += 1
+    else:
+        print("Sorry but I haven't heard of them.")
